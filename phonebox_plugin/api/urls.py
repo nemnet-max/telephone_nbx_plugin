9658f6043e06ca345c92f7b97e7943958dd069b0
nemtsev.pavel <nemtsev.pavel@uac-ssc.ru>
2022-07-11 10:18:22 +0700

    Change it into phonebox-plugin

--- phonebox_plugin/api/urls.py
+++ phonebox_plugin/api/urls.py
@@ -2,9 +2,9 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
-router.APIRootView = views.TelephoneNetBoxPluginRootView
+router.APIRootView = views.PhoneBoxPluginRootView
 
 router.register(r'numbers', views.NumberViewSet)
 
-app_name = "telephony_netbox_plugin-api"
+app_name = "phonebox_plugin-api"
 urlpatterns = router.urls
